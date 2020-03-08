15e230b7d2ec2a7d8cf7f76f6ca7555156b618ef
umairx97 <umair.bajwa@hotmail.com>
2020-03-09 03:00:31 +0500

    update

--- src/main.js
+++ src/main.js
@@ -6,18 +6,12 @@ import { configureStore } from '~/core/store';
 import nestPairs from '~/shared/nest-pairs';
 import Routes, { history } from '~/core/Router';
 
-// const language = navigator.language.split(/[-_]/)[0];
-// import { IntlProvider } from 'react-intl';
-// import { MuiThemeProvider } from '@material-ui/core/styles';
-// import i18n from '~/core/i18n';
-// import { theme } from '~/core/material';
-
 const store = configureStore();
+
+// Add Parent component with props 
 const Providers = nestPairs(
   [Provider, { store }],
   [ConnectedRouter, { history }],
-  // [IntlProvider, { locale: language, messages: i18n[language] }],
-  // [MuiThemeProvider, { theme }],
 );
 
 render(
